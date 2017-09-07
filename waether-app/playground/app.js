23facab5aa2bf362b65181d8fcd87054c6e5c23a
Dan Aronson <dan.ar27@gmail.com>
2017-09-07 15:51:22 +0300

    weather

--- waether-app/playground/app.js
+++
@@ -1,8 +0,0 @@
-const request = require('request');
-
-request({
-  url: "https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20st%20philadelphia",
-  json: true
-}, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
-});
